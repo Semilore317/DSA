9ce077331e6068be35e6de3ce85aa08348c2f150
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-05-27 10:37:07 +0100

    solved ContainsDUplicate optimally

--- java/leetcode/ContainsDuplicate.java
+++ java/leetcode/ContainsDuplicate.java
@@ -1,42 +1,22 @@
 // LeetCode #217 - Contains Duplicate
 
-import java.util.Arrays;
-/*
-217. Contains Duplicate
+import java.util.HashSet;
+import java.util.Set;
 
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
-Example 1:
-Input: nums = [1,2,3,1]
-Output: true
-Explanation:
-The element 1 occurs at the indices 0 and 3.
-
-Example 2:
-Input: nums = [1,2,3,4]
-Output: false
-Explanation:
-All elements are distinct.
-
-Example 3:
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
-* */
 public class ContainsDuplicate {
-    public boolean containsDuplicates(int[] nums){
-        Arrays.sort(nums);
-        int n = nums.length;
-        for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i-1]){
-                return true;
-            }
-        }
-        return false;
+  public static boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      if (!set.add(num)) { // returns false if item already exists
+        return true;
+      }
     }
 
-    public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
-        ContainsDuplicate solution = new ContainsDuplicate();
-        System.out.println(solution.containsDuplicates(nums));
-    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 1 };
+    System.out.println(containsDuplicate(arr));
+  }
 }
