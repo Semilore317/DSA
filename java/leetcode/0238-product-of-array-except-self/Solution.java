812d416b008dd989bbc3d8af256d9cd8332fa9cf
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-08-19 09:50:39 +0100

    solved problem 238 LC

---
+++ java/leetcode/0238-product-of-array-except-self/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int[] productExceptSelf(int[] nums) {
+    int n = nums.length;
+    int[] result = new int[n];
+
+    int left_product = 1;
+    for (int i = 0; i < n; i++) {
+      result[i] = left_product;
+      left_product *= nums[i];
+    }
+
+    int right_product = 1;
+    for (int i = n - 1; i >= 0; i--) {
+      result[i] *= right_product;
+      right_product *= nums[i];
+    }
+
+    return result;
+  }
+}
