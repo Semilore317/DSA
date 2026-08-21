49c56847cf582898ef1c5398580493fe3f1207ed
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-08-21 17:25:40 +0100

    binary tree level order traversal

--- java/leetcode/0102-binary-tree-level-order-traversal/Solution.java
+++ java/leetcode/0102-binary-tree-level-order-traversal/Solution.java
@@ -1,46 +1,70 @@
-// LeetCode #102 - Binary Tree Level Order Traversal
-
-import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.LinkedList;
 
-public class Solution {
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) {
-            this.val = val;
-        }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    // Level order traversal
+    List<List<Integer>> tree = new ArrayList<>();
+
+    if (root == null)
+      return tree;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+
+      List<Integer> currentLevel = new ArrayList<>();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+
+        // add the node first
+        currentLevel.add(node.val);
+        // add left and right if they exist then
+        if (node.left != null)
+          queue.add(node.left);
+        if (node.right != null)
+          queue.add(node.right);
+      }
+      tree.add(currentLevel);
     }
-
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            List<Integer> currentLevel = new ArrayList<>(size);
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                currentLevel.add(node.val);
-
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
-            }
-            result.add(currentLevel);
-        }
-        return result;
-    }
+    return tree;
+  }
 }
