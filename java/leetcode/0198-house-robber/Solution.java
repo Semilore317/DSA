6d13e6ba741b3c550ccf6421032671ddf975b7c9
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-08-21 17:19:22 +0100

    solved: house robber

---
+++ java/leetcode/0198-house-robber/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int rob(int[] nums) {
+    int[] dp = new int[nums.length + 1];
+    dp[0] = 0;
+    dp[1] = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      int x = nums[i];
+      dp[i + 1] = Math.max(dp[i], dp[i - 1] + x);
+    }
+
+    return dp[nums.length];
+  }
+}
